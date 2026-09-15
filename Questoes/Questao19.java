334d02808378ed9f52c7829d0556ae75d5bbfcdd
fortyqueen <pedro.jefferson1111@gmail.com>
2026-09-15 20:56:45 -0300

    Create Questao19.java

---
+++ Questoes/Questao19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Questao19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int invertido = 0;
+
+        while (numero != 0) {
+            int digito = numero % 10;
+            invertido = invertido * 10 + digito;
+            numero /= 10;
+        }
+
+        System.out.println("Número invertido: " + invertido);
+    }
+}
