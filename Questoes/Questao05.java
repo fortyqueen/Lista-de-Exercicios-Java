60c001c58cee7761dfb8c90b7d1f319323209325
fortyqueen <pedro.jefferson1111@gmail.com>
2026-09-15 20:45:29 -0300

    Create Questao05.java

---
+++ Questoes/Questao05.java
@@ -0,0 +1,21 @@
+public class Questao05 {
+    public static void main(String[] args) {
+        int quantidade = 0;
+
+        for (int i = 2; i <= 50; i++) {
+            int divisores = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisores++;
+                }
+            }
+
+            if (divisores == 2) {
+                quantidade++;
+            }
+        }
+
+        System.out.println("Quantidade de primos: " + quantidade);
+    }
+}
