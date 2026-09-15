3c3da51a585c73692af3224342d7d6b0a6c70a1d
fortyqueen <pedro.jefferson1111@gmail.com>
2026-09-15 20:55:28 -0300

    Create Questao15.java

---
+++ Questoes/Questao15.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Questao15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int original = numero;
+        int invertido = 0;
+
+        while (numero != 0) {
+            int digito = numero % 10;
+            invertido = invertido * 10 + digito;
+            numero = numero / 10;
+        }
+
+        if (original == invertido) {
+            System.out.println("É um palíndromo.");
+        } else {
+            System.out.println("Não é um palíndromo.");
+        }
+    }
+}
