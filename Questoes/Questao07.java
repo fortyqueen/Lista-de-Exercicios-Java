d08813e3ac28f9fbad0f6e3945485e662c378866
fortyqueen <pedro.jefferson1111@gmail.com>
2026-09-15 20:47:48 -0300

    Create Questao07.java

---
+++ Questoes/Questao07.java
@@ -0,0 +1,18 @@
+  public class Questao07 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 500; i++) {
+            int soma = 0;
+
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    soma += j;
+                }
+            }
+
+            if (soma == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
